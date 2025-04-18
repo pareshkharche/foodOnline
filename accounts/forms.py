5034d4bafa2159704909d550f20dad7def785c14
Paresh Kharche <pareshkharche1557@gmail.com>
2025-04-18 13:41:42 +0530

    vendor profile form validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import User, UserProfile
-# from .validators import allow_only_images_validator
+from .validators import allow_only_images_validator
 
 
 class UserForm(forms.ModelForm):
@@ -21,22 +21,22 @@ class UserForm(forms.ModelForm):
             )
 
 
-# class UserProfileForm(forms.ModelForm):
-#     address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Start typing...', 'required': 'required'}))
-#     profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
-#     cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Start typing...', 'required': 'required'}))
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
     
-#     # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
-#     # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
-#     class Meta:
-#         model = UserProfile
-#         fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
+    # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
 
-#     def __init__(self, *args, **kwargs):
-#         super(UserProfileForm, self).__init__(*args, **kwargs)
-#         for field in self.fields:
-#             if field == 'latitude' or field == 'longitude':
-#                 self.fields[field].widget.attrs['readonly'] = 'readonly'
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
 
 
 # class UserInfoForm(forms.ModelForm):
