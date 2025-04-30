d429cf382fe45f9ac208e507fbc050fa4b40dffa
Paresh Kharche <pareshkharche1557@gmail.com>
2025-04-30 20:49:06 +0530

    dynamic tax module

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -101,9 +101,10 @@ $(document).ready(function(){
                     //subtotal , tax, grand_total
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                     )
+                    console.log(response.cart_amount['tax_dict']);
                 }
             }
         })
@@ -144,7 +145,7 @@ $(document).ready(function(){
 
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                     )
 
@@ -159,7 +160,7 @@ $(document).ready(function(){
     })
 
 
-    //decrease cart
+    //// DELETE CART ITEM
     $('.delete_cart').on('click', function(e){
         e.preventDefault();
         // alert('test123');
@@ -181,7 +182,7 @@ $(document).ready(function(){
 
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                     )
 
@@ -210,12 +211,20 @@ $(document).ready(function(){
     }
 
 
-    //apply cart amounts
-    function applyCartAmounts(subtotal, tax, grand_total){
+    // apply cart amounts
+    function applyCartAmounts(subtotal, tax_dict, grand_total){
         if(window.location.pathname == '/cart/'){
             $('#subtotal').html(subtotal)
-            $('#tax').html(tax)
             $('#total').html(grand_total)
+
+            console.log(tax_dict)
+            for(key1 in tax_dict){
+                console.log(tax_dict[key1])
+                for(key2 in tax_dict[key1]){
+                    // console.log(tax_dict[key1][key2])
+                    $('#tax-'+key1).html(tax_dict[key1][key2])
+                }
+            }
         }
     }
 
--- marketplace/context_processors.py
+++ marketplace/context_processors.py
@@ -1,4 +1,4 @@
-from .models import Cart
+from .models import Cart, Tax
 from menu.models import FoodItem
 
 
@@ -16,6 +16,7 @@ def get_cart_counter(request):
             cart_count = 0
     return dict(cart_count=cart_count)
 
+
 def get_cart_amounts(request):
     subtotal = 0
     tax = 0
@@ -26,7 +27,14 @@ def get_cart_amounts(request):
         for item in cart_items:
             fooditem = FoodItem.objects.get(pk=item.fooditem.id)
             subtotal += (fooditem.price * item.quantity) # subtotal = subtotal + (fooditem.price * item.quantity)
+
+        get_tax = Tax.objects.filter(is_active=True)
+        for i in get_tax:
+            tax_type = i.tax_type
+            tax_percentage = i.tax_percentage
+            tax_amount = round((tax_percentage * subtotal)/100, 2)
+            tax_dict.update({tax_type: {str(tax_percentage) : tax_amount}})
+        
+        tax = sum(x for key in tax_dict.values() for x in key.values())
         grand_total = subtotal + tax
-    # print(subtotal)
-    # print(grand_total)
-    return dict(subtotal=subtotal, tax=tax, grand_total=grand_total)
+    return dict(subtotal=subtotal, tax=tax, grand_total=grand_total, tax_dict=tax_dict)
