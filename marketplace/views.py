767dc912ccb4fd66efe070ef6c965d9c6d1eba5d
Paresh Kharche <pareshkharche1557@gmail.com>
2025-05-03 02:29:25 +0530

    orders and paypal implentation

--- marketplace/views.py
+++ marketplace/views.py
@@ -14,12 +14,15 @@ from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from datetime import datetime, date, time
 
+from accounts.models import UserProfile
 
 from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.measure import D # ``D`` is a shortcut for ``Distance``
 from django.contrib.gis.db.models.functions import Distance
 
 from django.db.models import Prefetch
+
+from orders.forms import OrderForm
 
 from datetime import date
 # Create your views here.
@@ -207,3 +210,29 @@ def search(request):
 
 
         return render(request, 'marketplace/listings.html', context)
+
+@login_required(login_url='login')
+def checkout(request):
+    cart_items = Cart.objects.filter(user=request.user).order_by('created_at')
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        return redirect('marketplace')
+    
+    user_profile = UserProfile.objects.get(user=request.user)
+    default_values = {
+        'first_name': request.user.first_name,
+        'last_name': request.user.last_name,
+        'phone': request.user.phone_number,
+        'email': request.user.email,
+        'address': user_profile.address,
+        'country': user_profile.country,
+        'state': user_profile.state,
+        'city': user_profile.city,
+        'pin_code': user_profile.pin_code,
+    }
+    form = OrderForm(initial=default_values)
+    context = {
+        'form': form,
+        'cart_items': cart_items,
+    }
+    return render(request, 'marketplace/checkout.html', context)
